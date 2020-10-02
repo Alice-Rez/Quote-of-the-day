50d9dbaf4b80a95a3e142f024fd6d0a611fe7cc5
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-02 22:24:55 +0200

    cleaned comments from script

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// I created three different options of randomization :
-
-// 1.) low - it will display all of the elements in random order without repeating
-
-//2.) medium - there can be repetitions but not direct - next element will be everytime different that previous one
-
-// 3.) hight - totally random with possibility of repetition :)
-
 let board = document.getElementById("board");
 let author = document.getElementById("author");
 let buttonLow = document.getElementById("low-level");
@@ -24,7 +16,6 @@ fetch("https://philosophy-quotes-api.glitch.me/quotes")
   .then((jsonData) => {
     quotes = jsonData;
 
-    console.log(quotes);
     board.textContent = `${getQuote(quotes).quote}`;
     author.textContent = `${quotes[indexPrevious].source}`;
 
@@ -47,20 +38,8 @@ fetch("https://philosophy-quotes-api.glitch.me/quotes")
     });
   });
 
-// the main function is same, because the basics are the same
-
-// we just have another parameter, that decide, which level of randomization we want so which part of code will be additionally executed
-
-//also with high and medium randomization the indexes will be stored everytime in the array usedIndexes, because than I can switch to the low and effect is nearly immediate
-
-// same for indexPrevious variable
-
-// console logs just for checking
-
-function getQuote(array, level = "hight") {
+function getQuote(array, level = "high") {
   let index = chooseRandom(array);
-
-  console.log(indexPrevious, usedIndexes);
 
   if (usedIndexes.length === array.length) {
     usedIndexes = [];
@@ -68,12 +47,10 @@ function getQuote(array, level = "hight") {
 
   if (level === "medium") {
     while (index === indexPrevious) {
-      console.log("new index needed");
       index = chooseRandom(array);
     }
   } else if (level === "low") {
     while (usedIndexes.includes(index)) {
-      console.log("new index needed");
       index = chooseRandom(array);
     }
   }
@@ -82,12 +59,10 @@ function getQuote(array, level = "hight") {
 
   indexPrevious = index;
 
-  console.log(index, array[index]);
-
   return array[index];
 }
 
-// choosing of random number just amde in another function for clearness of code :)
+// choosing of random number just made in another function for clearness of code :)
 
 function chooseRandom(array) {
   return Math.floor(Math.random() * array.length);
